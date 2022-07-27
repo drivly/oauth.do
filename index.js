d5e260d5613dc807ecc62cbba3aa0583c27ae021
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2022-07-27 04:50:21 -0500

    Added login redirect

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { Router } from 'itty-router'
 import { error, json, withContent, withParams } from 'itty-router-extras'
+import { github, google } from 'worker-auth-providers'
 
 const router = Router()
 const recentInteractions = {}
@@ -17,15 +18,9 @@ const enrichRequest = req => {
 
 router.all('*', enrichRequest)
 router.get('/', (req, env) => json({ req }))
+router.get('/login', (req, env) => github.redirect({options:{clientId: env.GITHUB_CLIENT_ID}}))
 
 export default {
   fetch: router.handle
 }
-
-// app.get('/', (c) => c.json({hello: 'world'}))
-// app.get('/api', (c) => c.env.KV_API.list().then(data => c.json(data)))
-// app.get('/api/page/:cursor', (c) => c.env.KV_API.list(c.req.param).then(data => c.json(data)))
-// app.get('/api/:id', (c) => c.env.KV_API.get(c.req.param('id')).then(data => c.json(data)))
-// app.get('/api/put/:key/:value', (c) => c.env.KV_API.put(c.req.param('key'), c.req.param('value')).then(data => c.json(data)))
-
 
