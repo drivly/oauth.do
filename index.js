d299fc7c257a9cd95cd959c7cb7ae98f3b599921
Nathan Clevenger <nateclev@gmail.com>
2022-08-03 03:39:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ router.get('/callback', async (req, env) => {
     email: user.email,
   }
   
-  await env.USERS.put(user.id, JSON.stringify({profile,user}))
+  await env.USERS.put(user.id, JSON.stringify({profile,user}, null, 2))
   
   const claims = { user_id: user?.id }
   const secret = env.JWT_SECRET
